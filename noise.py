b46c79053ab2f3d6ae9e3d1a8703956664001dbe
Nader Elsayed <nadralssyd@gmail.com>
2026-03-09 01:02:44 +0200

    update the file name

--- noise.py
+++ noise.py
@@ -2,7 +2,7 @@ import librosa
 import noisereduce as nr
 import soundfile as sf
 
-audio, sr = librosa.load("input.wav", sr=None)
+audio, sr = librosa.load("DSP_task.wav", sr=None)
 reduced_noise = nr.reduce_noise(y=audio, sr=sr)
 sf.write("clean_audio.wav", reduced_noise, sr)
 print("Noise removed successfully")
